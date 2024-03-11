ef5fa8c42ff7fb197a6f24a9ebbd632c4efe480f
Lakshmi Javadekar <103459615+lakshmimsft@users.noreply.github.com>
2024-03-11 15:01:08 -0700

    added nginx image

--- main.tf
+++ main.tf
@@ -10,3 +10,17 @@ terraform {
 provider "docker" {
   host = "tcp://localhost:2375"
 }
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "nginx-test"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
