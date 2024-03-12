3a7f77d1201a77819909a32b5a9f2052a8e8301e
Lakshmi Javadekar <103459615+lakshmimsft@users.noreply.github.com>
2024-03-11 23:29:35 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = ">= 2.0"
     }
+    postgresql = {
+      source  = "cyrilgdn/postgresql"
+      version = "1.16.0"
+    }
   }
 }
 
