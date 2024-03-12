130fa1fcfb2c821bbc90e8a144ffd966010a8ff3
Lakshmi Javadekar <103459615+lakshmimsft@users.noreply.github.com>
2024-03-11 23:25:06 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,9 @@ variable "password" {
   description = "The password for the PostgreSQL database"
   type        = string
 }
+
+variable "context" {
+  description = "This variable contains Radius recipe context."
+
+  type = any
+}
