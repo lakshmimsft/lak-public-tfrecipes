5a6030ca5de2c4c5cd1a7fb72c1af5a3cb8817d7
Lakshmi Javadekar <103459615+lakshmimsft@users.noreply.github.com>
2024-03-11 22:18:03 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = ">= 2.0"
     }
+    postgresql = {
+      source  = "cyrilgdn/postgresql"
+      version = "1.16.0"
+    }
   }
 }
 
@@ -73,4 +77,33 @@ resource "kubernetes_service" "postgres" {
       target_port = 5432
     }
   }
+}
+
+terraform {
+  required_providers {
+    
+  }
+}
+
+variable "host" {
+  default = "postgres.dkr-resources-docker-default-recipe-app.svc.cluster.local"
+}
+
+variable "password" {
+  default = var.password
+}
+
+variable "port" {
+  default = 5432
+}
+
+provider "postgresql" {
+  host     = var.host
+  port     = var.port
+  password = var.password
+  sslmode  = "disable"
+}
+
+resource postgresql_database "pg_db_test" {
+  name = "pg_db_test"
 }
